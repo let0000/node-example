fdb3409da021c18d26bdf03a36c2f6cb365ddebe
JINHO LEE <wlsgh8309@naver.com>
2023-05-18 18:07:59 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -2,14 +2,41 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const { User } = require("./models/User");
+
+const config = require("./config/key");
+
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    `mongodb+srv://jignong2:time0904@jingnong2.aphoooe.mongodb.net/?retryWrites=true&w=majority`
-  )
+  .connect(config.mongoURI)
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => res.send("Hello World! 서버 열림"));
+
+app.post("/register", async (req, res) => {
+  //회원 가입 할때 필요한 정보들을 client 에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+
+  const user = new User(req.body);
+
+  await user
+    .save()
+    .then(() => {
+      res.status(200).json({
+        success: true,
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.json({
+        success: false,
+        err: err,
+      });
+    });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
