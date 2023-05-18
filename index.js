8dd1cfdeb5e3aea23333f9511f9a7a08539ad66d
JINHO LEE <wlsgh8309@naver.com>
2023-05-18 17:31:07 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    `mongodb+srv://jignong2:time0904@jingnong2.aphoooe.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
